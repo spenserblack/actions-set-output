70ad4ae8f74ca3d5c5e93e0fb0184e5feab1437f
Spenser Black <spenserblack01@gmail.com>
2021-09-07 14:28:53 -0400

    Set outputs

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,27 @@
 import * as core from '@actions/core';
+import { exec } from '@actions/exec';
+import { parseLine } from './parser';
 
-core.info('Hello, World!');
+const parsed = core.getInput('variables')
+  .split('\n')
+  .filter((line) => line.length)
+  .map(parseLine);
+
+const results = parsed.map(({ name, ...attrs }) => {
+  if ('value' in attrs) {
+    core.info(`Setting ${name}`);
+    core.setOutput(name, attrs.value);
+    return;
+  }
+
+  return exec(attrs.command, attrs.args, {
+    listeners: {
+      stdout: (data: Buffer) => {
+        core.info(`Setting ${name}`);
+        core.setOutput(name, data.toString());
+      },
+    },
+  });
+});
+
+Promise.all(results);
