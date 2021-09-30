670000beb90e0ab0e24806f9a76be950a8657acd
Spenser Black <spenserblack01@gmail.com>
2021-09-30 11:23:25 -0400

    Pass command to `/bin/sh -c`

--- __tests__/parser.test.ts
+++ __tests__/parser.test.ts
@@ -6,25 +6,6 @@ test('hardcoded value is detected', () => {
 test('command is detected', () => {
   expect(parseLine('VERSION=$(git describe --tags)')).toEqual({
     name: 'VERSION',
-    command: 'git',
-    args: ['describe', '--tags'],
+    command: 'git describe --tags',
   });
-});
-test('no command args means Command.args not defined', () => {
-  expect(parseLine('USER=$(whoami)').args).toHaveLength(0);
-});
-test('command args do not contain single quotes', () => {
-  const { args } = parseLine("GREETING=$(echo 'Hello')");
-  expect(args).toHaveLength(1);
-  expect(args[0]).toEqual('Hello');
-});
-test('command args do not contain double quotes', () => {
-  const { args } = parseLine('GREETING=$(echo "Hello")');
-  expect(args).toHaveLength(1);
-  expect(args[0]).toEqual('Hello');
-});
-test('quoted argument is one argument', () => {
-  const { args } = parseLine('GREETING=$(echo "Hello World")');
-  expect(args).toHaveLength(1);
-  expect(args[0]).toEqual('Hello World');
 });
